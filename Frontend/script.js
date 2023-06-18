289fe42b1e2ba8d89c9824ddd4d2f73a7008155a
saroj <sarojaryal2003@gmail.com>
2023-06-18 07:32:52 +0545

    homepage-update

--- Frontend/script.js
+++ Frontend/script.js
@@ -7,73 +7,69 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 
 // var marker = L.marker([27.67, 84.43]).addTo(map);
 
+
+let popup=document.getElementById('popup')
+let realMap=document.querySelector('#map')
+window.addEventListener('DOMContentLoaded',()=>{
+  realMap.style.display='none'
+  popup.style.display='block'
+
+  
+
+})
 //layer control
 let lati;
 let long;
 
-// navigator.geolocation.getCurrentPosition(function (position) {
-//   console.log(position);
-//   console.log(position.coords.latitude, position.coords.longitude);
-
-//   lat = position.coords.latitude
-//   lon = position.coords.longitude
-//   L.marker([lat, lon]).addTo(map)
-// })
-
-// L.flyTo([27.70169, 85.3206],10, {
-//   animate: true,
-//   duration: 3
-// }).addTo(map)
-
-// L.Routing.control({
-//   waypoints: [
-//     L.latLng(57.74, 11.94),
-//     L.latLng(57.6792, 11.949)
-//   ]
-// }).addTo(map);
-
-// L.marker([27.70169, 85.3206]).on('click', function() {
-//   console.log('add');
-// });
-
-// map.on("click", function(e){
-//   new L.Marker([e.latlng.lat, e.latlng.lng]).addTo(map)
-
-//   navigator.geolocation.getCurrentPosition(function(position){
-//     lat = position.coords.latitude
-//     lon = position.coords.longitude
-//   })
-
-//   L.Routing.control({
-//     waypoints: [
-//       L.latLng(e.latlng.lat,e.latlng.lng),
-//       L.latLng(lat, lon)
-//     ]
-//   }).addTo(map);
-// })
 
 let placeData = L.geoJSON(mapPlace).addTo(map);
 
-const popUp = document.querySelector(".popup");
-const selected = document.getElementById("selected");
+const popUp = document.querySelector(".detials");
+const mapContainer = document.getElementById("map");
+// const selected = document.getElementById("selected");
+const navigateBtn = document.getElementById("navigateBtn")
+navigateBtn.onclick = ()=>{
+  startNavigation()
+}
+
 placeData.on("click", function (e) {
   lati = e.latlng.lat;
   long = e.latlng.lng;
+  navigateBtn.innerText = "Start Navigation"
+  mapContainer.style.width = "70%";
+  popUp.style.display = "block";
+  const station_title = document.getElementById("title");
+  const station_description = document.getElementById("station_description");
+  const address = document.getElementById("address");
+  const connectivity = document.getElementById("connectivity");
+  const charging_level = document.getElementById("charging_level");
+  const availibility = document.getElementById("availibility");
+  const no_of_cords = document.getElementById("no_of_cords");
+
+// station_title.innerText = e.
+console.log(e.layer.feature.properties);
+const data = e.layer.feature.properties;
+station_title.innerText = data.name
+station_description.innerText =  data.description
+connectivity.innerText ="Connectivity: " + data.connectivity
+charging_level.innerText ="Charging Level: "+ data.charging_level;
+availibility.innerText = "Availibility: " + data.availibility;
+no_of_cords.innerText ="Number of Cords: " + data.curds
+address.innerText  = data.address
 
   // popUp.style.display = "block";
-  popUp.classList.add("popupAnim");
+  // popUp.classList.add("popupAnim");
 
   // L.tooltip(e.latlng,).addTo(map);
 });
 
 function startNavigation() {
-  console.log("ljk");
   navigator.geolocation.getCurrentPosition(({ coords }) => {
     console.log(coords);
+    navigateBtn.innerText='Stop Navigation'
     L.Routing.control({
       waypoints: [
         L.latLng(coords.latitude, coords.longitude),
-
         L.latLng(lati, long),
       ],
     }).addTo(map);
